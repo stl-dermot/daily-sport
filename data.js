e6dfad3802d55fea1026fb7ee181f9d91796656f
dermot <dermot@sentinel-it.net>
2026-07-06 10:09:14 +0800

    feat: source thumbnails from data

--- data.js
+++ data.js
@@ -4,6 +4,7 @@ window.dailySportEntries = [
     date: "2026-07-03",
     title: "30 MIN NO JUMPING HIIT CARDIO - ALL STANDING Workout - No Equipment - No Repeat, Low Impact",
     url: "https://www.youtube.com/watch?v=HuYoYJX9pgU",
+    thumbnail: "https://i.ytimg.com/vi/HuYoYJX9pgU/hqdefault.jpg",
     description: "這支30分鐘的燃脂操全程「站著做」，完全不用跳、也不用任何器材，膝蓋和地板都能鬆一口氣（樓下鄰居謝謝你）！強度走中等HIIT路線，深蹲、弓箭步、核心動作通通有，招式不重複超新鮮，不管平常有沒有運動習慣的同事都能輕鬆跟著一起流汗～"
   },
   {
@@ -11,6 +12,7 @@ window.dailySportEntries = [
     date: "2026-07-03",
     title: "30 MIN LOW IMPACT FULL BODY - No Repeats, No Jumping, No Equipment (Warm Up & Cool Down Included)",
     url: "https://www.youtube.com/watch?v=5LwN80lJvZc",
+    thumbnail: "https://i.ytimg.com/vi/5LwN80lJvZc/hqdefault.jpg",
     description: "今天換一支全身燃脂操，一樣全程站著、零跳躍、零器材，膝蓋友善指數依舊滿分！強度屬於中等有氧+肌力混合，動作不重複，還貼心附上暖身跟收操，新手老手都能輕鬆跟上，男女同事都適合一起流汗～"
   }
 ];
